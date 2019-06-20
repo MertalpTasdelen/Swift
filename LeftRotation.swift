974aa2cd2e7c87f35bc3d4d8daf8db2f026478d0
Mertalp <32788629+MertalpTasdelen@users.noreply.github.com>
2019-06-21 01:05:51 +0300

    Update LeftRotation
    code is now get a success rate of %100

--- LeftRotation.swift
+++ LeftRotation.swift
@@ -3,12 +3,8 @@ import Foundation
 // Complete the rotLeft function below.
 func rotLeft(a: [Int], d: Int) -> [Int] {
     var tempArray = a
-    for _ in 0 ..< d {
-        var temp = tempArray[0]
-        for i in 0 ..< tempArray.count-1 {
-            tempArray[i] = tempArray[i + 1]
-        }
-    tempArray[tempArray.count - 1] = temp
-}
-    return tempArray
+    var lastPart = tempArray[0..<d]
+    var firstPart = tempArray[d..<tempArray.count]
+    var newArray = Array(firstPart + lastPart)
+    return newArray
 }
